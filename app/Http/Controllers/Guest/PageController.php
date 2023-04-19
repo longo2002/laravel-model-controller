359956686b4a89401a51a7271ced30d24d79e603
longo2002 <lombardo.fra2002@gmail.com>
2023-04-19 19:13:39 +0200

    Bonus

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -1,11 +1,28 @@
 <?php
 
 namespace App\Http\Controllers\Guest;
-
+use App\Models\Movie;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+   public function homepage()
+   {
+
+    $movies = Movie::all();
+    dd($movies);
+    $data = [
+        "movies" => $movies
+    ];
+
+    return view("welcome");
+   }
+
+   public function contacts()
+   {
+    return view("guest.contacts");
+   } 
+   
+   
 }
